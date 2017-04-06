0aec6ab279ceccbcf4ff5fa3080173bc084cfdaf
Assen Kolov <assen.kolov@gmail.com>
2017-04-06 22:45:55 +0200

    deploy

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -3,7 +3,7 @@
 # Exit on any error
 set -e
 
-sudo /opt/google-cloud-sdk/bin/gcloud docker push us.gcr.io/${PROJECT_NAME}/where2fly2
+sudo /opt/google-cloud-sdk/bin/gcloud docker push eu.gcr.io/${PROJECT_NAME}/where2fly2
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 kubectl patch deployment docker-hello-google -p '{"spec":{"template":{"spec":{"containers":[{"name":"where2fly2",
 "image":"eu.gcr.io/iconic-setup-91510/where2fly2:latest"}]}}}}'
