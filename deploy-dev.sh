2bd22888e7678f73bce82875a65bfd908031968b
Assen Kolov <assen.kolov@gmail.com>
2017-04-07 18:23:44 +0200

    deployment name was wrong

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -4,5 +4,5 @@
 set -e
 
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
-kubectl patch deployment docker-hello-google -p '{"spec":{"template":{"spec":{"containers":[{"name":"where2fly2",
+kubectl patch deployment where2fly2 -p '{"spec":{"template":{"spec":{"containers":[{"name":"where2fly2",
 "image":"eu.gcr.io/iconic-setup-91510/where2fly2:latest"}]}}}}'
