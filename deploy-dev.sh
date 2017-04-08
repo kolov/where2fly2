78527ac61405ccdfe9bbdbd36efef5401a69663b
Assen Kolov <assen.kolov@gmail.com>
2017-04-09 00:09:39 +0200

    deploy-dev

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -2,6 +2,9 @@
 
 # Exit on any error
 set -e
-
+echo Starting deployment
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
-kubectl patch deployment where2fly2 -p '{"spec":{"template":{"spec":{"containers":[{"name":"where2fly2", "image":"eu.gcr.io/iconic-setup-91510/where2fly2:latest"}]}}}}'
+#kubectl patch deployment where2fly2 -p '{"spec":{"template":{"spec":{"containers":[{"name":"w2f2", "image":"eu.gcr
+#.io/iconic-setup-91510/where2fly2:latest"}]}}}}'
+kubectl delete deployment w2f2
+kubectl apply -f docker/kube/w2f2-d.yml
