bcea7ad8f4ec31d81a9050b7a8fcf7d3fcbf81dd
Assen Kolov <assen.kolov@gmail.com>
2017-04-09 11:06:22 +0200

    skip build

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -8,7 +8,9 @@ set GOOGLE_APPLICATION_CREDENTIALS=$HOME/gcloud-service-key.json
 echo $GOOGLE_APPLICATION_CREDENTIALS
 cat $GOOGLE_APPLICATION_CREDENTIALS
 sudo /opt/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
-echo Activated
+echo Activated 1
+sudo /opt/google-cloud-sdk/bin/gcloud auth application-default login
+echo Activated 2
 #kubectl patch deployment where2fly2 -p '{"spec":{"template":{"spec":{"containers":[{"name":"w2f2", "image":"eu.gcr
 #.io/iconic-setup-91510/where2fly2:latest"}]}}}}'
 kubectl delete deployment w2f2
