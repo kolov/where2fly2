65713b7335d16cacd91cadacf7a25ef2fdb579a3
Assen Kolov <assen.kolov@gmail.com>
2017-04-09 10:53:14 +0200

    GOOGLE_APPLICATION_CREDENTIALS

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -4,7 +4,11 @@
 set -e
 echo Starting deployment
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
-set GOOGLE_APPLICATION_CREDENTIALS =$HOME/gcloud-service-key.json
+set GOOGLE_APPLICATION_CREDENTIALS=$HOME/gcloud-service-key.json
+echo $GOOGLE_APPLICATION_CREDENTIALS
+cat $GOOGLE_APPLICATION_CREDENTIALS
+sudo /opt/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+echo Activated
 #kubectl patch deployment where2fly2 -p '{"spec":{"template":{"spec":{"containers":[{"name":"w2f2", "image":"eu.gcr
 #.io/iconic-setup-91510/where2fly2:latest"}]}}}}'
 kubectl delete deployment w2f2
